b5f2dd9e17e4fed66176df203fe6819a6828f7e0
moon4433 <johnk117@ferris.edu>
2023-05-02 00:45:10 -0400

    Setting up the game

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let score0 = 0;
+let score1 = 0;
